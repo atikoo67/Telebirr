c7f54e7b16308f4fe41b4010cbcb65e340a84b72
atikoo67 <atinafbedasaa@gmail.com>
2026-01-01 22:19:40 +0300

    adding features for app page

--- lib/presentation/screens/apps_page.dart
+++ lib/presentation/screens/apps_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:telebirr/core/theme/theme.dart';
+import 'package:telebirr/presentation/widgets/squarecard.dart';
 
 class AppsPage extends StatefulWidget {
   const AppsPage({super.key});
@@ -18,6 +19,36 @@ class _AppsPageState extends State<AppsPage> {
         centerTitle: true,
         automaticallyImplyLeading: false,
         actions: [Icon(Icons.search)],
+      ),
+      backgroundColor: theme.colorScheme.shadow,
+      body: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.all(15.0),
+            sliver: SliverGrid.builder(
+              itemCount: 21,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                childAspectRatio: 1,
+              ),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 3),
+                  child: SquareCard(
+                    image: Image.asset(
+                      'assets/ethiotelecom logo.jpg',
+
+                      cacheHeight: 45,
+                    ),
+                    title: "My ethiotel",
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
