ac4133095356655ec5bc8781bfdc2adc09644d78
atikoo67 <atinafbedasaa@gmail.com>
2026-01-06 00:50:18 +0300

    add some list of tele services

--- lib/core/constant/lists.dart
+++ lib/core/constant/lists.dart
@@ -1,3 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:telebirr/core/theme/theme.dart';
+
 class AppLists {
   static final List<String> languages = ["English", "አማርኛ", "ትግርኛ", "Oromo"];
+}
+
+class ExampleServices {
+  static final List services = [
+    [
+      "Send Money",
+      Icon(Icons.payment_rounded, color: theme.colorScheme.primary),
+    ],
+    [
+      "Cash In/Out",
+      Icon(
+        Icons.account_balance_wallet_outlined,
+        color: theme.colorScheme.primary,
+      ),
+    ],
+    [
+      "Airtime buy Package",
+      Icon(Icons.payment_rounded, color: theme.colorScheme.primary),
+    ],
+    [
+      "Zemen Gebeya",
+      Icon(Icons.receipt_long_sharp, color: theme.colorScheme.primary),
+    ],
+    [
+      "Financial service with dashen",
+      Icon(Icons.account_balance_outlined, color: theme.colorScheme.primary),
+    ],
+    [
+      "Financial service with CBE",
+      Icon(Icons.home_mini_rounded, color: theme.colorScheme.primary),
+    ],
+  ];
 }
