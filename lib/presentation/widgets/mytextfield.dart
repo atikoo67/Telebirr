74a6377dbb01c5ec6b9ed081b76edbed71e4e875
atikoo67 <atinafbedasaa@gmail.com>
2026-01-07 22:08:13 +0300

    update a custom textfield

--- lib/presentation/widgets/mytextfield.dart
+++ lib/presentation/widgets/mytextfield.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 class MyTextField extends StatelessWidget {
   final String label;
   final TextEditingController? controller;
+  Color? borderColor;
+  Widget? suffix;
 
-  const MyTextField({super.key, required this.label, required this.controller});
+  MyTextField({
+    super.key,
+    required this.label,
+    required this.controller,
+    this.borderColor,
+    this.suffix,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +23,16 @@ class MyTextField extends StatelessWidget {
       style: theme.textTheme.titleSmall,
       decoration: InputDecoration(
         border: OutlineInputBorder(
-          borderSide: BorderSide(color: theme.colorScheme.tertiary),
+          borderSide: BorderSide(
+            color: borderColor ?? theme.colorScheme.tertiary,
+          ),
           borderRadius: BorderRadius.all(Radius.circular(8)),
         ),
         prefixIcon: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Text('+251', style: theme.textTheme.titleSmall),
         ),
+        suffix: suffix ?? null,
         prefixIconConstraints: BoxConstraints(minWidth: 0, minHeight: 0),
         // hintText: hintText,
         contentPadding: EdgeInsets.symmetric(horizontal: 5, vertical: 13),
